8ae4919c4cc29881a951a04cb4d16fa2f1ee0308
jonschipp <jonschipp@gmail.com>
2013-02-04 18:21:17 -0800

    Added: firewall support and wget/curl checks

--- mal-dnssearch.sh
+++ mal-dnssearch.sh
@@ -13,6 +13,7 @@ Compare DNS logs against known mal-ware host list
 	-a 	ARGUS file
         -b      BRO-IDS dns.log file
         -d      Tcpdump pcap file
+	-f      insert firewall rules e.g. iptables,pf,ipfw
 	-h      help (this)
 	-i	ISC's BIND query log file 
         -l      Log stdout & stderr to file
@@ -23,8 +24,8 @@ Compare DNS logs against known mal-ware host list
         -w      Whitelist, accept file or argument
                 e.g. -w "dont|match|these"
 
-Usage: $0 [option] logfile [-w whitelist] [-l output.log]
-e.g. $0 -p /var/log/pdns.log -w "facebook|google" -l output.log
+Usage: $0 [option] logfile [-w whitelist] [ -f fw ][-l output.log]
+e.g. $0 -p /var/log/pdns.log -w "facebook|google" -f iptables -l output.log
 EOF
 }
 
@@ -44,6 +45,23 @@ echo "grep -v -i -E '(in-addr|$WLISTDOM)'"
 fi
 }
 
+ipblock()
+{
+if [ "$FW" == "iptables" ]; then
+iptables -A INPUT -s "$bad_host" -j DROP
+iptables -A OUTPUT -s "$bad_host" -j DROP
+iptables -A FORWARD -s "$bad_host" -j DROP
+fi
+if [ "$FW" == "pf" ]; then
+echo -e "block in from yahoo.com to any\n \
+block out from yahoo.com to any" | pfctl -a mal-dnssearch -f -
+fi
+if [ "$FW" == "ipfw" ]; then
+ipfw add drop ip from "$bad_host" to any
+ipfw add drop ip from any to "$bad_host"
+fi
+}
+
 compare()
 {
 echo -e "\n[*] Results - ${FILE}: comparing $logttl entries\n"
@@ -55,6 +73,11 @@ for host in $(eval "$1")
 do
 if [ "$bad_host" == "$host" ]; then
 echo "[+] Found - host '"$host"' matches "
+
+	if [ "$FWTRUE" == 1 ]; then
+	ipblock
+	fi
+
 let found++
 break
 fi
@@ -64,35 +87,39 @@ done < <(cut -f1 < malhosts.txt | sed -e '/^#/d' -e '/^$/d')
 echo -e "--\n[=] $found of $total entries matched from malhosts.txt\n"
 }
 
-# test for argument
+# if less than 1 argument
 if [ ! $# -gt 1 ]; then
 usage
 exit 1
 fi
 
 # option and argument handling
-while getopts "hb:d:i:l:p:o:s:t:w:" OPTION
+while getopts "hb:d:f:i:l:p:o:s:t:w:" OPTION
 do
      case $OPTION in
 	 a)
 	     ARGUS=1
-	     FILE="$OPTARG"
+	     ARGUSFILE="$OPTARG"
 	     ;;
          b)
              BRO=1
-             FILE="$OPTARG"
+             BROFILE="$OPTARG"
              ;;
 	 d) 
              TCPDUMP=1
-             FILE="$OPTARG"
+             TCPDUMPFILE="$OPTARG"
              ;;
+	 f)
+	     FWTRUE=1
+	     FW="$OPTARG"
+	     ;;
          h)
              usage
              exit 1
              ;;
 	 i) 
 	     BIND=1
-	     FILE="$OPTARG"
+	     BINDFILE="$OPTARG"
 	     ;; 
          l)
              LOG=1
@@ -100,19 +127,19 @@ do
              ;;
          p)
              PDNS=1
-             FILE="$OPTARG"
+             PDNSFILE="$OPTARG"
              ;;
 	 o) 
 	     SWALL=1
-	     FILE="$OPTARG"
+	     SWALLFILE="$OPTARG"
 	     ;;
 	 s) 
     	     TSHARK=1
-  	     FILE="$OPTARG"
+  	     TSHARKFILE="$OPTARG"
 	     ;;
 	 t)
 	     HTTPRY=1
-             FILE="$OPTARG"
+             HTTPRYFILE="$OPTARG"
 	     ;;
          w)
              WLISTDOM="$OPTARG"
@@ -125,14 +152,21 @@ done
 
 echo -e "\nPID: $$"
 
-# d/l malhost list
-curl -O https://secure.mayhemiclabs.com/malhosts/malhosts.txt &>/dev/null
+# check for curl/wget and then d/l malhost list
+if command -v curl >/dev/null 2>&1; then
+curl -O http://secure.mayhemiclabs.com/malhosts/malhosts.txt &>/dev/null
+elif command -v wget >/dev/null 2>&1; then
+wget http://secure.mayhemiclabs.com/malhosts/malhosts.txt &>/dev/null
+else
+echo -e "\nERROR: Neither cURL or Wget are installed or are not in the \$PATH!\n"
+exit 1
+fi
 
 # vars
 tally=0
 found=0
 total=$(sed -e '/^$/d' -e '/^#/d' < malhosts.txt | wc -l)
-logttl=$(wc -l $FILE | awk '{ print $1 }')
+#logttl=$(wc -l $FILE | awk '{ print $1 }')
 
 # logging
 if [ "$LOG" == 1 ]; then
@@ -142,31 +176,31 @@ fi
 
 # meat
 if [ "$BRO" == 1 ]; then
-compare "bro-cut query < \$FILE | $(eval wlistchk) | sort | uniq"
+compare "bro-cut query < \$BROFILE | $(eval wlistchk) | sort | uniq"
 fi
 if [ "$PDNS" == 1 ]; then
-compare "sed 's/||/:/g' < \$FILE | $(eval wlistchk) | cut -d \: -f5 | sed 's/\.$//' | sort | uniq"
+compare "sed 's/||/:/g' < \$PDNSFILE | $(eval wlistchk) | cut -d \: -f5 | sed 's/\.$//' | sort | uniq"
 fi
 if [ "$HTTPRY" == 1 ]; then
-compare "awk '{ print $7 }' < \$FILE | $(eval wlistchk) | sed -e '/^-$/d' -e '/^$/d' | sort | uniq"
+compare "awk '{ print $7 }' < \$HTTPRYFILE | $(eval wlistchk) | sed -e '/^-$/d' -e '/^$/d' | sort | uniq"
 fi
 if [ "$TSHARK" == 1 ]; then
-compare "tshark -nr \$FILE -R udp.port==53 -e dns.qry.name -T fields 2>/dev/null \
+compare "tshark -nr \$TSHARKFILE -R udp.port==53 -e dns.qry.name -T fields 2>/dev/null \
 | $(eval wlistchk) | sed -e '/#/d' | sort | uniq"
 fi
 if [ "$TCPDUMP" == 1 ]; then
-compare "tcpdump -nnr \$FILE udp port 53 2>/dev/null | grep -o 'A? .*\.' | $(eval wlistchk) \
+compare "tcpdump -nnr \$TCPDUMPFILE udp port 53 2>/dev/null | grep -o 'A? .*\.' | $(eval wlistchk) \
  | sed -e 's/A? //' -e '/[#,\)\(]/d' -e '/^[a-zA-Z0-9].\{1,4\}$/d' -e 's/\.$//'| sort | uniq"
 fi
 if [ "$ARGUS" == 1 ]; then
-compare "ra -nnr \$FILE -s suser:512 - udp port 53 | $(eval wlistchk) | \
+compare "ra -nnr \$ARGUSFILE -s suser:512 - udp port 53 | $(eval wlistchk) | \
 sed -e 's/s\[..\]\=.\{1,13\}//' -e 's/\.\{1,20\}$//' -e 's/^[0-9\.]*$//' -e '/^$/d' | sort | uniq"
 fi
 if [ "$BIND" == 1 ]; then
-compare "awk '/query/ { print \$15 } /resolving/ { print \$13 }' \$FILE | $(eval wlistchk) \ 
+compare "awk '/query/ { print \$15 } /resolving/ { print \$13 }' \$BINDFILE | $(eval wlistchk) \ 
 | grep -v resolving | sed -e 's/'\"'\"'//g' -e 's/\/.*\/.*://' -e '/[\(\)]/d' | sort | uniq"
 fi 
 if [ "$SWALL" == 1 ]; then
-compare "grep -h -o 'dstname=.* a' \$FILE 2>/dev/null | $(eval wlistchk) \
+compare "grep -h -o 'dstname=.* a' \$SWALLFILE 2>/dev/null | $(eval wlistchk) \
 | sed -e 's/dstname=//' -e 's/ a.*//' | sort | uniq"
 fi 
