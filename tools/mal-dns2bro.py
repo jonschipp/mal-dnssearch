a87b4f395a91cb5370e084ec543c02ec123f4cd9
Aaron Eppert <aaron.eppert@packetsled.com>
2015-09-29 09:44:24 -0400

    Only return what we are given for headers and contents

--- tools/mal-dns2bro.py
+++ tools/mal-dns2bro.py
@@ -3,7 +3,9 @@
 # Aaron Eppert
 # September 2015
 #
-
+# September 28, 2015        Initial Release                             Aaron Eppert
+# September 29, 2015        Dynamic header creation and field filling   Aaron Eppert
+#
 import os
 import re
 import sys
@@ -22,12 +24,11 @@ fmt.format_description = decorate(fmt.format_description)
 
 
 class mal_dns2bro:
-    HEADER = "#fields\tindicator\tindicator_type\tmeta.source\tmeta.url\tmeta.do_notice\tmeta.if_in\tmeta.whitelist\tmeta.desc\tmeta.cif_severity\tmeta.cif_impact"
-
-
     def __init__(self, args_dict):
         self.args_dict = args_dict
         self.append_intel_line = None
+        self.sorted_hdr = [(0, '#fields',   None),
+                           (1, 'indicator', None)]
 
         self.s2l_types = {'ip':        'Intel::ADDR',
                           'domain':    'Intel::DOMAIN',
@@ -64,8 +65,30 @@ class mal_dns2bro:
                       'SSL::IN_SERVER_NAME',
                       'SMTP::IN_HEADER']
 
+        self.option_to_header = [('#fields',         '#fields',             lambda : None),
+                                 ('indicator',       'indicator',           lambda : None),
+                                 ('type',            'indicator_type',      self.__type),
+                                 ('source',          'meta.source',         self.__source),
+                                 ('url',             'meta.url',            self.__url),
+                                 ('notice',          'meta.do_notice',      self.__notice),
+                                 ('if_in',           'meta.if_in',          self.__if_in),
+                                 ('whitelist',       'meta.whitelist',      self.__whitelist),
+                                 ('desc',            'meta.desc',           self.__desc),
+                                 ('cif_severity',    'meta.cif_severity',   self.__cif_severity),
+                                 ('cif_impact',      'meta.cif_impact',     self.__cif_impact),
+                                 ('cif_confidence',  'meta.cif_confidence', self.__confidence)]
+
     def __verify_chars(self, t):
         return all(ord(l) > 31 and ord(l) < 127 and l in string.printable for l in t)
+
+    def __find_header_order(self, t):
+        ret = -1
+        try:
+            ret = map(lambda x: x[0], self.option_to_header).index(t)
+        except ValueError:
+            print 'ERROR: Invalid header!'
+            sys.exit(1)
+        return ret
 
     def __cif_severity(self):
         ret = ''
@@ -74,7 +97,7 @@ class mal_dns2bro:
             ret = self.args_dict['cif_severity']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('cif_severity'), ret)
 
     def __cif_impact(self):
         ret = ''
@@ -82,7 +105,7 @@ class mal_dns2bro:
             ret = self.args_dict['cif_impact']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('cif_impact'), ret)
 
     def __desc(self):
         ret = ''
@@ -90,7 +113,7 @@ class mal_dns2bro:
             ret = self.args_dict['desc']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('desc'), ret)
 
     def __if_in(self):
         ret = ''
@@ -98,7 +121,7 @@ class mal_dns2bro:
             ret = self.args_dict['if_in']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('if_in'), ret)
 
     def __notice(self):
         ret = 'F'
@@ -106,7 +129,7 @@ class mal_dns2bro:
                    'false': 'F'}
         if self.args_dict['notice'] is not None and _to_bro.get(self.args_dict['notice'], None) is not None:
             ret = _to_bro.get(self.args_dict['notice'])
-        return ret
+        return (self.__find_header_order('notice'), ret)
 
     def __type(self):
         ret = ''
@@ -118,7 +141,7 @@ class mal_dns2bro:
             else:
                 sys.stderr.write('ERROR: TYPE not specified!\n')
                 sys.exit(1)
-        return ret
+        return (self.__find_header_order('type'), ret)
 
     def __source(self):
         ret = ''
@@ -126,7 +149,7 @@ class mal_dns2bro:
             ret = self.args_dict['source']
         else:
             ret = 'mal-dnssearch'
-        return ret
+        return (self.__find_header_order('source'), ret)
 
     def __url(self):
         ret = ''
@@ -134,7 +157,7 @@ class mal_dns2bro:
             ret = self.args_dict['url']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('url'), ret)
 
     def __whitelist(self):
         ret = ''
@@ -142,7 +165,18 @@ class mal_dns2bro:
             ret = self.args_dict['whitelist']
         else:
             ret = '-'
-        return ret
+        return (self.__find_header_order('whitelist'), ret)
+
+    def __confidence(self):
+        ret = None
+        if self.args_dict['cif_confidence'] is not None and len(self.args_dict['cif_confidence']) > 0:
+            try:
+                t_int = int(self.args_dict['cif_confidence'])
+                if isinstance(t_int, (int, long)) and (t_int > 0 and t_int < 100):
+                    ret = str(t_int)
+            except ValueError:
+                ret = None
+        return (self.__find_header_order('cif_confidence'), ret)
 
     def __in_whitelist(self, t):
         ret = False
@@ -160,20 +194,31 @@ class mal_dns2bro:
         return ret
 
     def __prep_append_intel_line(self):
-        self.append_intel_line = '\t'.join([self.__type(),
-                                            self.__source(),
-                                            self.__url(),
-                                            self.__notice(),
-                                            self.__if_in(),
-                                            self.__whitelist(),
-                                            self.__desc(),
-                                            self.__cif_severity(),
-                                            self.__cif_impact()])
+        self.append_intel_line = '\t'.join([t[2]()[1] for t in self.sorted_hdr[2:]])
+
+    def __gen_header(self):
+        ret = ''
+        t_args_dict_to_field_name = map(lambda x: x[0], self.option_to_header)
+        for k in self.args_dict.keys():
+            if self.args_dict[k] is not None:
+                try:
+                    t_index = t_args_dict_to_field_name.index(k)
+                    self.sorted_hdr.append((t_index, self.option_to_header[t_index][1], self.option_to_header[t_index][2]))
+                except ValueError:
+                    pass
+
+        if len(self.sorted_hdr) > 0:
+            self.sorted_hdr.sort(key=lambda x: x[0])
+            ret = '\t'.join(map(lambda x: x[1], self.sorted_hdr))
+        else:
+            sys.stderr.write('ERROR: Failed to generate header')
+            sys.exit(1)
+        return ret
 
     def format(self):
         t_fd = self.__file()
         if t_fd is not None:
-            print self.HEADER
+            print self.__gen_header()
             self.__prep_append_intel_line()
 
             for line in t_fd:
@@ -187,7 +232,8 @@ class mal_dns2bro:
 def main():
     parser = OptionParser()
     parser.add_option('-f', dest='file', help='Read parsed list from file (if option is ommited, use stdin)')
-    parser.add_option('-g', dest='cif_severity', help='meta.cif_severity')
+    parser.add_option('-g', dest='cif_severity', help="""Reported Severity: 'low', 'medium', 'med', 'high'""")
+    parser.add_option('-c', dest='cif_confidence', help="""Confidence percentage - 0...100""")
     parser.add_option('-k', dest='cif_impact', help='meta.cif_impact')
     parser.add_option('-d', dest='desc', help='Description of entry (meta.desc)')
     parser.add_option('-i', dest='if_in', help='Location seen in Bro (def: null)')
